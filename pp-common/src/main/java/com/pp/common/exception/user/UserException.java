745bc6c56656401ed90471206e0aa400012fe0d4
panyuzhe <2735439993@qq.com>
2024-10-12 13:18:18 +0800

    工作流依赖整合

---
+++ pp-common/src/main/java/com/pp/common/exception/user/UserException.java
@@ -0,0 +1,18 @@
+package com.pp.common.exception.user;
+
+import com.pp.common.exception.base.BaseException;
+
+/**
+ * 用户信息异常类
+ * 
+ * @author ruoyi
+ */
+public class UserException extends BaseException
+{
+    private static final long serialVersionUID = 1L;
+
+    public UserException(String code, Object[] args)
+    {
+        super("user", code, args, null);
+    }
+}
